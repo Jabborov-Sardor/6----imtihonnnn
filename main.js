70aa44f25abbcacee9f2c356cf4385dd1f4b527c
Jabborov-Sardor <jabborovsardor0097.net@gmail.com>
2024-09-16 09:03:20 +0500

    Yakuniy

---
+++ main.js
@@ -0,0 +1,34 @@
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    fetch("https://fakestoreapi.com/auth/login", {
+      method: "POST",
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        if (json.token) {
+          localStorage.setItem("token", json.token);
+          window.location.href = "admin.html";
+          document.getElementById("username").value = ""
+          document.getElementById("password").value = "";
+        } else {
+          alert("Login yoki parol xato!");
+        }
+        
+      })
+      .catch((err) => {
+        console.error("Xato:", err);
+        alert("Tizimda muammo yuz berdi. Qayta urinib ko'ring.");
+      });
+  });
